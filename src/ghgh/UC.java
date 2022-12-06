8d3c953ed890860364c7a9ee7b4d16a33e455c78
aluno <aluno@172.16.3.32>
2022-12-06 17:09:29 -0300

    uc quase feito

--- src/ghgh/UC.java
+++ src/ghgh/UC.java
@@ -3,17 +3,24 @@ package ghgh;
 public class UC {
 	
 	private int ucid;
-	//cliente
+	private Cliente cliente;
 	private int leituraAnterior;
 	private int leituraAnual;
 	private Endereco endereco;
 	
-	public UC(int ucid, int leituraAnterior, int leituraAnual, Endereco endereco) {
+	public UC(int ucid, int leituraAnterior, int leituraAnual, Endereco endereco, Cliente cliente) {
 		this.ucid = ucid;
 		this.endereco = endereco;
 		this.leituraAnterior = leituraAnterior;
 		this.leituraAnual = leituraAnual;
-
+		this.cliente = cliente;
+		
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 	public int getUcid() {
 		return ucid;
@@ -39,5 +46,23 @@ public class UC {
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
+	public String toString() {
+		String texto = "";
+		
+		texto = "-------------------------------------------"
+				+"\n UCID: " + getUcid()
+				+"\n Cliente: " + getCliente()
+				+"\n Leitura Anterior: " + getLeituraAnterior()
+				+"\n Leitura Atual: " + getLeituraAnual()
+				+"\n Endereço: " + getEndereco();
+		
+		return texto;
+	}
+	public int faturar(int leitura) {
+		leitura += leituraAnterior + leituraAnual;
+		return leitura;
+		
+	}
+	
 	public 
 }
